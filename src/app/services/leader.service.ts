1a22c7c004b78fd3bc1d7eddcad129125af1ce72
Parth Shrivastava <parth.shrivastava11@gmail.com>
2018-07-11 01:30:23 +0530

    Angular and Promise Part 2

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -11,14 +11,20 @@ export class LeaderService {
   constructor() { }
 
   getLeaders(): Promise<Leader[]> {
-    return Promise.resolve(Leaders);
+    return new Promise(resolve=> {
+      setTimeout(() => resolve(Leaders), 2000);
+    });
   }
 
   getLeader(id: number): Promise<Leader> {
-    return Promise.resolve(Leaders.filter((leader) => (leader.id === id))[0]);
+    return new Promise(resolve=> {
+      setTimeout(() => resolve(Leaders.filter((leader) => (leader.id === id))[0]), 2000);
+    });
   }
 
   getFeaturedLeader(): Promise<Leader> {
-    return Promise.resolve(Leaders.filter((leader) => (leader.featured))[0]);
+    return  new Promise(resolve=> {
+      setTimeout(() => resolve(Leaders.filter((leader) => leader.featured)[0]), 2000);
+    });
   }
 }
